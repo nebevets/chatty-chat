e21f8587520cbe7ab2fb4bf9779350a96b136de1
nebevets <steveben@exmsft.com>
2019-08-12 17:19:47 -0700

    folder structure started

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import Home from './home';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
-        </div>
+        <Home/>
     </div>
 );
 
---
+++ src/components/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+  return(
+    <h1>chat home</h1>
+  );
+}
+
+export default Home;
+
+// video stopped at 25.15
